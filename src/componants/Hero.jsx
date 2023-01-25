4d95295e117bc6a29199a8ff1240ee5e77c4ec7c
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-25 22:14:45 +0100

    hero text

--- src/componants/Hero.jsx
+++ src/componants/Hero.jsx
@@ -4,11 +4,26 @@ function Hero() {
   return (
     <section>
         <div className="p-6 text-green-50 bg-emerald-800">
-            <h1 className="text-green-500 text-2xl">Nguyen Dev: Créer un site web n'a jamais été aussi facile</h1>
-            <p className="pt-4">Site internet sur mesure vitrine</p>
-            <p className="pt-2">Rapide, Sécurisé et Facile</p>
-            <p className="pt-2">Domaine, SSL, code barre </p>
-            <p className="pt-2">À partir de 500 €</p>
+            <h1 className="text-green-500 text-3xl">Nguyen Dev: Créer un site web n'a jamais été aussi facile</h1>
+            <div className="md:flex">
+                <div className="max-w-xs p-4">
+                    <h2 className="text-xl">Site internet sur mesure vitrine</h2>
+                    <p className="pt-3">Tout comme les vêtements, tu ne veux personne comme toi</p>
+                    <p className="">Garantie votre site est unique</p>
+                </div>
+                <div className="max-w-xs p-4">
+                    <h2 className="text-xl">Rapide, Sécurisé et Facile</h2>
+                    <p className="pt-3">un jour de A-Z si vous avez deja votre idea</p>
+                    <p className="">Utilisation des dernières technologies de sécurité</p>
+                    <p className="">Dites-moi simplement vos idées et je vous aiderai à les mettre en œuvre</p>
+                </div>
+                <div className="max-w-xs p-4">
+                    <h2 className="text-xl">À partir de 500 €</h2>
+                    <p className="pt-3">Oui,Je ne plaisante pas, essayez-moi </p>
+                    <p className="">Inclus: domain, code, logo, design, code barre...</p>
+                    <p className="">La meilleure qualité avec le prix que vous pouvez payer</p>
+                </div>
+            </div>
         </div>
 
         <div className="">
