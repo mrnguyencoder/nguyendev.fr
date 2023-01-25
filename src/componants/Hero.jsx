ba2d5252bd494dc59359fb372b8a87aee79528a8
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-25 16:44:53 +0100

    hero

--- src/componants/Hero.jsx
+++ src/componants/Hero.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section>
+        <h1 className="">Nguyen Dev: Créer un site web n'a jamais été aussi facile</h1>
+        <p className="">Site internet sur mesure vitrine</p>
+        <p className="">Rapide, Sécurisé et Facile</p>
+        <p className="">Domaine, SSL, code barre </p>
+        <p className="">À partir de 500 €</p>
+
+        <img src="" alt="" />
+    </section>
   )
 }
 
