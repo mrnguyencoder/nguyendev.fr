bedfd1629eae20184fd8f7810a087a29e86ec8a3
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-25 17:48:36 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Hero from './componants/Hero';
+import Clients from './componants/Clients';
+import Contact from './componants/Contact';
+import NavBar from './componants/NavBar';
+import Footer from './componants/Footer';
 
 function App() {
   return (
-    <div className="App">
-    <p className="">Test</p>
-    </div>
+    <>
+    <NavBar />
+    <Routes>
+      <Route path='/' element={<Hero />} />
+      <Route path='/clients' element={<Clients />} />
+      <Route path='/contact' element={<Contact />} />
+
+    </Routes>
+    <Footer />
+    
+    </>
   );
 }
 
--- src/componants/Hero.jsx
+++ src/componants/Hero.jsx
@@ -3,13 +3,17 @@ import React from 'react'
 function Hero() {
   return (
     <section>
-        <h1 className="">Nguyen Dev: Créer un site web n'a jamais été aussi facile</h1>
-        <p className="">Site internet sur mesure vitrine</p>
-        <p className="">Rapide, Sécurisé et Facile</p>
-        <p className="">Domaine, SSL, code barre </p>
-        <p className="">À partir de 500 €</p>
+        <div className="">
+            <h1 className="">Nguyen Dev: Créer un site web n'a jamais été aussi facile</h1>
+            <p className="">Site internet sur mesure vitrine</p>
+            <p className="">Rapide, Sécurisé et Facile</p>
+            <p className="">Domaine, SSL, code barre </p>
+            <p className="">À partir de 500 €</p>
+        </div>
 
-        <img src="" alt="" />
+        <div className="">
+            <img src="" alt="" />
+        </div>
     </section>
   )
 }
