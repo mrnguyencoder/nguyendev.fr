b021ee610dac6b6b7151f3df690a6ac8bebb8230
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-26 22:06:24 +0100

    set up nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      <p className="text-red-600">Hello </p>
+      <NavBar/>
+      <Hero/>
+      <Footer/>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/iconnguyencoder.png';
+import { } from '@heroicons/react/24/outline';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className='bg-slate-800 w-full xl:max-w-[1280px] overflow-hidden
+                    flex justify-between items-center'>
+        <img src={logo} alt="" className='h-10' />
+
+        <ul className="hidden md:flex items-center space-x-4 text-green-50 pr-4">
+            <li className=""><a href="">Accueil</a></li>
+            <li className=""><a href="">Propos</a></li>
+            <li className=""><a href="">Clients</a></li>
+            <li className=""><a href="">Contact</a></li>
+        </ul>
+
+        <div className="md:hidden">
+            <img src="" alt="" className="" />
+        </div>
+    </nav>
   )
 }
 
