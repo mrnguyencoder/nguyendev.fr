0dad2d800f65461a9fb4cd853a366f1445292798
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-29 22:07:57 +0100

    hero and create faqs

---
+++ src/components/FAQs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Disclosure } from '@headlessui/react'
+import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline';
+
+function FAQs() {
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+export default FAQs
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React from 'react';
+
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section className="">
+          <div className="bg-white">
+      <div className="mx-auto max-w-7xl sm:px-6 sm:py-32 lg:px-8">
+        <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+          <h2 className="mx-auto max-w-2xl text-4xl font-bold tracking-tight text-white">
+          Création de Site Internet Sur Mesure
+          </h2>
+          <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
+          Le coût d’un site web vitrine pour une petite entreprise commence à environ 350€.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-[#25d366] px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            >
+              WhatsApp-moi
+            </a>
+            <a href="#" className="text-base font-semibold leading-7 text-white">
+              En savoir plus <span aria-hidden="true">→</span>
+            </a>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1024 1024"
+            className="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
+            aria-hidden="true"
+          >
+            <circle cx={512} cy={512} r={512} fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fillOpacity="0.7" />
+            <defs>
+              <radialGradient
+                id="827591b1-ce8c-4110-b064-7cb85a0b1217"
+                cx={0}
+                cy={0}
+                r={1}
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(512 512) rotate(90) scale(512)"
+              >
+                <stop stopColor="#7775D6" />
+                <stop offset={1} stopColor="#E935C1" stopOpacity={0} />
+              </radialGradient>
+            </defs>
+          </svg>
+        </div>
+      </div>
+    </div>
+    </section>
   )
 }
 
