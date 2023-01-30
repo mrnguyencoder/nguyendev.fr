84e26e5a6126ef6028513b8204ab5f4ff029caa7
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-01-30 15:34:37 +0100

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -51,7 +51,7 @@ function Hero() {
     <div className="bg-gray-50 pt-12 sm:pt-16">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-4xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="text-3xl font-bold tracking-tight text-[#F0DB4F] sm:text-4xl">
           Clients dans tous les domaines
           </h2>
           <p className="mt-3 text-xl text-gray-500 sm:mt-4">
@@ -67,15 +67,15 @@ function Hero() {
               <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
                 <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
                   <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Le Responsive Web Design</dt>
-                  <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">100%</dd>
+                  <dd className="order-1 text-5xl font-bold tracking-tight text-[#61dbfb]">100%</dd>
                 </div>
                 <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
                   <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Service</dt>
-                  <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">24/7</dd>
+                  <dd className="order-1 text-5xl font-bold tracking-tight text-[#61dbfb]">24/7</dd>
                 </div>
                 <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
                   <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Réponse</dt>
-                  <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">1J</dd>
+                  <dd className="order-1 text-5xl font-bold tracking-tight text-[#61dbfb]">1J</dd>
                 </div>
               </dl>
             </div>
